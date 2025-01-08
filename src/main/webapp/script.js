134401a2e00f231c6f2f1ff707412d7bb9849dfb
DEEPAK KUMAR D <98271570+Deepakd1006@users.noreply.github.com>
2025-01-09 02:44:34 +0530

    Update script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Welcome to Deepak Kumar D's page!");
+    const contentMessage = document.querySelector(".content p");
+
+    const messages = [
+        "Empowering businesses through cloud innovation.",
+        "Streamlining CI/CD pipelines for better efficiency.",
+        "Driving scalable and resilient cloud solutions.",
+        "Harnessing the power of DevOps for business growth."
+    ];
+
+    let index = 0;
+    setInterval(() => {
+        contentMessage.textContent = messages[index];
+        index = (index + 1) % messages.length;
+    }, 3000);
 });
